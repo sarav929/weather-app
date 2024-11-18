16690f9f1b42ea1039a7d5fe0b62d92eae681e5c
Sara <123300249+sarav929@users.noreply.github.com>
2024-11-18 11:37:37 +0000

    add input to submit location to API query

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import './styles.css'
 
-const key = '88GTR4YP9S7U2QHX337V2YRPE'
-const location = 'London,UK'
-const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}`
+function createForm() { 
+    const body = document.querySelector('body')
+    const form = document.createElement('form')
+    form.setAttribute('id', 'search-location')
+
+    body.appendChild(form)
+
+    const search = document.createElement('input')
+    search.type = 'text'
+    search.setAttribute('id', 'location')
+    search.required = true
+
+    form.appendChild(search)
+
+    const searchBtn = document.createElement('button')
+    searchBtn.type = 'submit'
+    searchBtn.textContent = 'Search'
+
+    form.appendChild(searchBtn)
+}
+
+function getLocation(input) {
+    return input.value
+}
 
 async function getWeather() {
+    const key = '88GTR4YP9S7U2QHX337V2YRPE'
+    const search = document.getElementById('location')
+    const location = getLocation(search)
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}`
     try {
         const response = await fetch(url, {mod: 'CORS'})
         if(!response.ok) {
@@ -18,4 +43,12 @@ async function getWeather() {
 
 }
 
-getWeather()
+createForm()
+
+const form = document.getElementById('search-location')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    getWeather()
+})
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,45 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js', 
-  output: {
-    path: path.resolve(__dirname, 'dist'), 
-    filename: 'bundle.js', 
-  },
-  mode: 'development', 
-  module: {
-    rules: [
-      {
-        test: /\.js$/, 
-        exclude: /node_modules/, 
-        use: {
-          loader: 'babel-loader', 
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true, 
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    devServer: {
+        static: "./dist",
+        port: 3000, 
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html",
+        }),
     ],
-  },
-  devServer: {
-    static: './dist',
-    port: 3000, 
-  },
 };
