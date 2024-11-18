a9a572577ca5bad19c6950d97b420de45550a585
Sara <123300249+sarav929@users.noreply.github.com>
2024-11-18 11:07:24 +0000

    console log weather info

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './styles.css'
 
-console.log('test')
+const key = '88GTR4YP9S7U2QHX337V2YRPE'
+const location = 'London,UK'
+const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}`
+
+async function getWeather() {
+    try {
+        const response = await fetch(url, {mod: 'CORS'})
+        if(!response.ok) {
+            console.error('Error with response')
+        }
+        const weatherData = await response.json()
+        console.log(weatherData)
+    } catch(error) {
+        console.error('Cought an error')
+    }
+
+}
+
+getWeather()
