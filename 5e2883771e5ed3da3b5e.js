94dc76d263b8f66b0b8a4b872ff1a919f791436b
Sara <123300249+sarav929@users.noreply.github.com>
2024-11-25 10:54:24 +0000

    Updates

---
+++ 5e2883771e5ed3da3b5e.js
@@ -0,0 +1,1 @@
+import"./styles/homepage.css";import"./styles/weatherpage.css";import{getWeatherByLocation}from"./weather";getWeatherByLocation();
--- ce68c8075db2bf02896c.js
+++
@@ -1,4 +0,0 @@
-import './styles/homepage.css';
-import './styles/weatherpage.css';
-import { getWeatherByLocation } from './weather';
-getWeatherByLocation();
